0f2ff1ca7e0d11d1713511961eb25d2c2f06b638
Xinguang Wang <wang1xin@police.nsw.gov.au>
2024-03-15 12:40:53 +1100

    test

--- start-ollama.sh
+++ start-ollama.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Start Ollama in the background
-/usr/local/bin/ollama serve &
+/usr/bin/ollama serve &
 
 # Wait a bit for the server to start (adjust the sleep time as needed)
 sleep 10
 
 # Pull the desired model. Replace 'llama2' with your model of choice
-/usr/local/bin/ollama pull llama2 &
+/usr/bin/ollama pull llama2 &
 
 sleep 10
 
-/usr/local/bin/ollama run llama2 
+/usr/bin/ollama run llama2 
 
 
 # Keep the script running to keep the container alive.
